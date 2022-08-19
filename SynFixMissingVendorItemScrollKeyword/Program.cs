16c5585bcc29a62bb916bca382807f7711bc3947
TokcDK <TokcDK@gmail.com>
2022-08-19 10:29:24 +0500

    added base code

--- SynFixMissingVendorItemScrollKeyword/Program.cs
+++ SynFixMissingVendorItemScrollKeyword/Program.cs
@@ -1,6 +1,7 @@
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Plugins;
 
 namespace SynFixMissingVendorItemScrollKeyword
 {
@@ -10,13 +11,28 @@ namespace SynFixMissingVendorItemScrollKeyword
         {
             return await SynthesisPipeline.Instance
                 .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
-                .SetTypicalOpen(GameRelease.SkyrimSE, "YourPatcher.esp")
+                .SetTypicalOpen(GameRelease.SkyrimSE, "SynFixMissingVendorItemScrollKeyword.esp")
                 .Run(args);
         }
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            //Your code here!
+            var vendorItemScrollFormKey = FormKey.Factory("0A0E57:Skyrim.esm");// VendorItemScroll [KYWD:000A0E57]
+
+            int patchedCount = 0;
+            foreach (var scroolGetter in state.LoadOrder.PriorityOrder.Scroll().WinningOverrides())
+            {
+                if (scroolGetter.Keywords != null && scroolGetter.Keywords.Contains(vendorItemScrollFormKey)) continue;
+
+                patchedCount++;
+
+                var scrollToPatch = state.PatchMod.Scrolls.GetOrAddAsOverride(scroolGetter);
+                if (scrollToPatch.Keywords == null) scrollToPatch.Keywords = new Noggog.ExtendedList<IFormLinkGetter<IKeywordGetter>>();
+
+                scrollToPatch.Keywords.Add(vendorItemScrollFormKey);
+            }
+
+            Console.WriteLine($"Fixed {patchedCount} records");
         }
     }
 }
