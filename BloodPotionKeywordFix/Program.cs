92ca504b2ba76aa544c7c1a18b7f411febc39b8c
mrslanddrops <73705365+mrslanddrops@users.noreply.github.com>
2024-10-10 18:34:22 -0500

    Update Program.cs

--- BloodPotionKeywordFix/Program.cs
+++ BloodPotionKeywordFix/Program.cs
@@ -24,7 +24,7 @@ int patchedCount = 0;
             foreach (var potionGetter in state.LoadOrder.PriorityOrder.Ingestible().WinningOverrides())
             {
                if (potionGetter.Keywords != null && potionGetter.Keywords.Contains(vendorItemPotionFormKey)) continue;
-               if (potionGetter.FormType(17)) continue;
+               if (potionGetter.Name("Potion of Blood")) continue;
 
                 patchedNpcCount++;
               
