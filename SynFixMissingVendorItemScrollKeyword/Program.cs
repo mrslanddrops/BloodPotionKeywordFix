67fe264a781182f43f47bf4172a02bd9d0ef0a29
mrslanddrops <73705365+mrslanddrops@users.noreply.github.com>
2024-10-10 12:56:21 -0500

    Update Program.cs

--- SynFixMissingVendorItemScrollKeyword/Program.cs
+++ SynFixMissingVendorItemScrollKeyword/Program.cs
@@ -3,7 +3,7 @@ using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Plugins;
 
-namespace SynFixMissingVendorItemScrollKeyword
+namespace BloodPotionKeywordFix
 {
     public class Program
     {
@@ -11,25 +11,24 @@ namespace SynFixMissingVendorItemScrollKeyword
         {
             return await SynthesisPipeline.Instance
                 .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
-                .SetTypicalOpen(GameRelease.SkyrimSE, "SynFixMissingVendorItemScrollKeyword.esp")
+                .SetTypicalOpen(GameRelease.SkyrimSE, "BloodPotionKeywordFix.esp")
                 .Run(args);
         }
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            var vendorItemScrollFormKey = FormKey.Factory("0A0E57:Skyrim.esm");// VendorItemScroll [KYWD:000A0E57]
-
+            var vendorItemPotionFormKey = FormKey.Factory("08CDEC:Dawnguard.esm");// VendorItemPotion [KYWD:0008CDEC]
             int patchedCount = 0;
-            foreach (var scroolGetter in state.LoadOrder.PriorityOrder.Scroll().WinningOverrides())
+            foreach (var scroolGetter in state.LoadOrder.PriorityOrder.Potion().WinningOverrides())
             {
-                if (scroolGetter.Keywords != null && scroolGetter.Keywords.Contains(vendorItemScrollFormKey)) continue;
+                if (potionGetter.Keywords != null && potionGetter.Keywords.Contains(vendorItemPotionFormKey)) continue;
 
                 patchedCount++;
 
-                var scrollToPatch = state.PatchMod.Scrolls.GetOrAddAsOverride(scroolGetter);
-                if (scrollToPatch.Keywords == null) scrollToPatch.Keywords = new Noggog.ExtendedList<IFormLinkGetter<IKeywordGetter>>();
+                var potionToPatch = state.PatchMod.Potions.GetOrAddAsOverride(potionGetter);
+                if (potionToPatch.Keywords == null) potionToPatch.Keywords = new Noggog.ExtendedList<IFormLinkGetter<IKeywordGetter>>();
 
-                scrollToPatch.Keywords.Add(vendorItemScrollFormKey);
+                potionToPatch.Keywords.Add(vendorItemPotionFormKey);
             }
 
             Console.WriteLine($"Fixed {patchedCount} records");
