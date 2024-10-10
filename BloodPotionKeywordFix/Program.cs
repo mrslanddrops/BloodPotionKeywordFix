d484f51ae6e9466f2d55d0ae280b8499919347be
mrslanddrops <73705365+mrslanddrops@users.noreply.github.com>
2024-10-10 18:45:02 -0500

    Update Program.cs

--- BloodPotionKeywordFix/Program.cs
+++ BloodPotionKeywordFix/Program.cs
@@ -2,6 +2,7 @@ using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Plugins;
+using SkyrimInterfaceAssetType;
 
 namespace BloodPotionKeywordFix
 {
