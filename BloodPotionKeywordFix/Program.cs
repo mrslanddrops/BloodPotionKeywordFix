abd05403f00b37be74eab28decd4f035baa5ad3d
mrslanddrops <73705365+mrslanddrops@users.noreply.github.com>
2024-10-10 18:04:39 -0500

    Update Program.cs

--- BloodPotionKeywordFix/Program.cs
+++ BloodPotionKeywordFix/Program.cs
@@ -39,9 +39,9 @@ int patchedCount = 0;
         }
                     
              static readonly FormKey bloodPotionFormKey = FormKey.Factory("018EF4:Dawnguard.esm");
-        private static bool IsValidPotion(potionGetter, IPatcherState<ISkyrimMod, ISkyrimModGetter> state);
+        private static bool IsValidPotion(IPatcherState<ISkyrimMod, ISkyrimModGetter> state);
         {
-            if (npcGetter.EditorID != null && npcGetter.EditorID(bloodPotionFormKey)) return false;
+            if (potionGetter.EditorID != null && potionGetter.EditorID(bloodPotionFormKey)) return false;
               return true;
         }
 }
