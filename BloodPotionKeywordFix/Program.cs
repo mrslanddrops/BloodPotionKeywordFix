184e4ba6eafac19959accfff3351a1d57cda71d0
mrslanddrops <73705365+mrslanddrops@users.noreply.github.com>
2024-10-10 14:28:06 -0500

    Update Program.cs

--- BloodPotionKeywordFix/Program.cs
+++ BloodPotionKeywordFix/Program.cs
@@ -1,37 +1,37 @@
-using System;
-using System.Threading.Tasks;
-
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Plugins;
 
-using Noggog;
-
 namespace BloodPotionKeywordFix
-
 {
-    public class Program
-    {
-        public static async Task<int> Main(string[] args)
-        {
-            return await SynthesisPipeline.Instance
-                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
-                .SetTypicalOpen(GameRelease.SkyrimSE, "BloodPotionKeywordFix.esp")
-                .Run(args);
-        }
+public class Program
+{
+public static async Task<int> Main(string[] args)
+{
+return await SynthesisPipeline.Instance
+.AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+.SetTypicalOpen(GameRelease.SkyrimSE, "BloodPotionKeywordFix.esp")
+.Run(args);
+}
 
-        public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
-        {
-            state.LoadOrder.PriorityOrder.OnlyEnabled().Ingestible().WinningOverrides().ForEach(alch =>
-            {
-                if (alch.HasKeyword(Keyword.VendorItemPotion) || alch.HasKeyword(Keyword.VendorItemPoison))
-                {
-                    Console.WriteLine($"Patching {alch.Name}");
-                    var nalch = state.PatchMod.Ingestibles.GetOrAddAsOverride(alch);
-                    nalch = FormKey.Factory("08CDEC:Dawnguard.esm");// VendorItemPotion [KYWD:0008CDEC]
-                }
-            });
-        }
-    }
+public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+{
+var vendorItemPotionFormKey = FormKey.Factory("08CDEC:Dawnguard.esm");// VendorItemPotion [KYWD:0008CDEC]
+int patchedCount = 0;
+            foreach (var potionGetter in state.LoadOrder.PriorityOrder.OnlyEnabled().Ingestible().WinningOverrides()
+{
+if (potionGetter.Keywords != null && potionGetter.Keywords.Contains(vendorItemPotionFormKey)) continue;
+
+patchedCount++;
+
+var potionToPatch = state.PatchMod.Ingestible.GetOrAddAsOverride(potionGetter);
+if (potionToPatch.Keywords == null) potionToPatch.Keywords = new Noggog.ExtendedList<IFormLinkGetter<IKeywordGetter>>();
+
+potionToPatch.Keywords.Add(vendorItemPotionFormKey);
+}
+
+Console.WriteLine($"Fixed {patchedCount} records");
+}
+}
 }
