327a5380d067b996922e91804e5d74eb1f149a7a
mrslanddrops <73705365+mrslanddrops@users.noreply.github.com>
2024-10-10 18:57:58 -0500

    Update Program.cs

--- BloodPotionKeywordFix/Program.cs
+++ BloodPotionKeywordFix/Program.cs
@@ -8,7 +8,8 @@ namespace BloodPotionKeywordFix
 public class Program
 {
 public static async Task<int> Main(string[] args);
-   {  public partial interface IItemGetter : ISkyrimMajorRecordGetter
+   {  
+      public partial interface IItemGetter : ISkyrimMajorRecordGetter
    {}
 {
 return await SynthesisPipeline.Instance
